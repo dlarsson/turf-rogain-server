a728aabf112bf0539ff8355b621bf328de11c400
Daniel Larsson <daniel.larsson@grouptalk.com>
2019-06-10 14:22:48 +0200

    Set NODE_ENV if not set

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 process.env.MONGODB = 'mongodb+srv://admin:zBAjDRAuS5KVmi2@paltn01-qtyrk.mongodb.net/test?retryWrites=true';
-
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = 'development';
+}
 require('./dist/src/index');
